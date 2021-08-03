9f824a4111490fb99ed10b0bc2560c8fa3f7d12c
alexei <alexei.jobs@gmail.com>
2021-08-03 12:25:02 +0300

    Add getTableColumn func

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,6 +1,8 @@
 package sample;
 
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import sample.datamodel.Word;
 import sample.datamodel.WordsData;
@@ -12,14 +14,21 @@ public class Controller {
 
     private WordsData data;
 
-    public void initialize(){
+    public void initialize() {
         data = new WordsData();
         data.loadDict();
         dictionaryTable.setItems(data.getWords());
         dictionaryTable.setStyle("-fx-font-size:22px;");
         //dictionaryTable.setStyle("-fx-background-color: #1d1d1d;");
+
     }
 
+    // Get TableColumnByName :)
+    private <T> TableColumn<T, ?> getTableColumnByName(TableView<T> tableView, String name) {
+        for (TableColumn<T, ?> col : tableView.getColumns())
+            if (col.getText().equals(name)) return col ;
+        return null ;
+    }
 
 
 }
